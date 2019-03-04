17687e4ab5cad4b014f4b3187acd92bd9250ce51
Kira_Klim <dimaisin@yandex.ru>
2019-03-04 22:17:41 +0300

    lab2 done

--- Heron's formula1/src/Main.java
+++ Heron's formula1/src/Main.java
@@ -6,19 +6,19 @@ public class Main {
     public static void main(String[] args) {
         boolean exit = false;
         DecimalFormat df = new DecimalFormat("#.###");
-        while (!exit) {
+
             System.out.println("Введите число, к которому необходимо найти корень");
             Scanner scan = new Scanner(System.in);
             double orig_num = scan.nextDouble();
             double estimated_num = scan.nextDouble();
             double result = 0;
             double cnt = estimated_num;
-            while (Math.abs(result-cnt) > 0.1) {
+            while (Math.abs(result-cnt) > 0.01) {
                 result = 0.5*(estimated_num+(orig_num/estimated_num));
                 cnt = estimated_num;
                 estimated_num = result;
             }
             System.out.println(df.format(result));
-        }
+
     }
 }
